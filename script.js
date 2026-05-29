f11273f4e8800170de087a86c927bc9d1a38ba2b
Charvi Aggarwal <153435560+CharviAggarwal02@users.noreply.github.com>
2026-05-29 16:12:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function sendMessage() {
 
     displayMessage(userInput, 'userMessage');
 
-    fetch('http://127.0.0.1:5000/chat', {
+    fetch('/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ message: userInput })
